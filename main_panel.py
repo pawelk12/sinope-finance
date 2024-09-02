ddb52920dd7ddaffe803784f635f21e30690821d
pawelk12 <pawel.krempa12@gmail.com>
2024-09-02 14:02:52 +0200

    Main panel interface enhancement

--- main_panel.py
+++ main_panel.py
@@ -1,6 +1,7 @@
 import time
 import customtkinter as ctk
 from account import Account
+from tkinter import PhotoImage
 from fund_transfer import TransferWidgets
 from transfer_history import TransferHistoryWidgets
 from db_service import GetData, GetTransferHistory, CheckSavings
@@ -17,7 +18,7 @@ class MainWidgets(ctk.CTkFrame):
 
         self.mainFrame._border_width = 3
         self.mainFrame._border_color = "#3d9bd7"
-        self.mainFrame._corner_radius = 32
+        self.mainFrame._corner_radius = 10
 
 
         self.balanceLabel = ctk.CTkLabel(self.mainFrame, text="{:.2f}".format(self.account.Balance) ,font=("Arial",80),
@@ -27,33 +28,68 @@ class MainWidgets(ctk.CTkFrame):
         self.timeLabel = ctk.CTkLabel(self.mainFrame,font=('Arial',20),
                                    fg_color="transparent")
         self.savingsButton = ctk.CTkButton(self.mainFrame,
-                                           text="Savings Deposits"
-                                           ,command=self.savingsDeposits)
+                                           text="Savings Deposits",
+                                           fg_color="transparent",
+                                           corner_radius=30,
+                                           border_width=2,
+                                           border_spacing=6,
+                                           border_color="#3d9bd7",
+                                           command=self.savingsDeposits)
 
         self.updateBalance()
         self.checkSavings()
         self.updateTime()
-        self.timeLabel.pack(anchor="ne")
+        self.timeLabel.pack(anchor="ne",padx=15,pady=5)
         self.balanceLabel.pack(anchor="s", expand=True)
         self.currencyLabel.pack(anchor="n", expand=True)
-        self.savingsButton.pack()
+        self.savingsButton.pack(pady=30)
 
 
         ############### Bar frame for pages
 
         self.barFrame = ctk.CTkFrame(self)
 
-        transferButton = ctk.CTkButton(self.barFrame, text="Transfer of funds",command=self.fundTransfer)
-        transferButton.pack()
+        pathToUserIcon = "resources/welcome/user.png"
+        userIconImage = PhotoImage(file=pathToUserIcon)
+        userImageLabel = ctk.CTkLabel(self.barFrame,text="",image=userIconImage)
+        userImageLabel.pack()
 
-        historyButton = ctk.CTkButton(self.barFrame, text="Transfer history",command=self.transferHistory)
-        historyButton.pack()
+        transferButton = ctk.CTkButton(self.barFrame, text="Transfer of funds",
+                                           fg_color="transparent",
+                                           corner_radius=8,
+                                           border_width=2,
+                                           border_spacing=8,
+                                           border_color="#3d9bd7",
+                                           command=self.fundTransfer)
+        transferButton.pack(ipadx=30,pady=0)
 
-        accountButton = ctk.CTkButton(self.barFrame, text="My account",command=self.accountWidgets)
-        accountButton.pack()
+        historyButton = ctk.CTkButton(self.barFrame, text="Transfer history",
+                                           fg_color="transparent",
+                                           corner_radius=8,
+                                           border_width=2,
+                                           border_spacing=8,
+                                           border_color="#3d9bd7",
+                                           command=self.transferHistory)
+        historyButton.pack(ipadx=30,pady=0)
 
-        logoutButton = ctk.CTkButton(self.barFrame, text="Log out",command=self.logOut)
-        logoutButton.pack(anchor="s",expand = True)
+        accountButton = ctk.CTkButton(self.barFrame, text="My account",
+                                           fg_color="transparent",
+                                           corner_radius=8,
+                                           border_width=2,
+                                           border_spacing=8,
+                                           border_color="#3d9bd7",
+                                           command=self.accountWidgets)
+        accountButton.pack(ipadx=30,pady=0)
+
+        logoutButton = ctk.CTkButton(self.barFrame, text="Log out",
+                                            fg_color="transparent",
+                                            corner_radius=8,
+                                            border_width=2,
+                                            border_spacing=8,
+                                            border_color="#ed460e",
+                                            hover_color="#632410",
+                                            command=self.logOut)
+        logoutButton.pack(anchor="s",expand = True,ipadx=30)
 
         ############### Packing frames and stuff
         self.barFrame.pack(side=ctk.LEFT,fill=ctk.Y)
@@ -61,7 +97,7 @@ class MainWidgets(ctk.CTkFrame):
         self.pack(fill=ctk.BOTH, expand=True)
 
     def updateTime(self):
-        time_str = time.strftime("%H:%M:%S   %d-%m-%Y")
+        time_str = time.strftime("%H:%M:%S   %d.%m.%Y")
         self.timeLabel.configure(text=time_str)
         self.timeLabel.after(1000,self.updateTime)
 
