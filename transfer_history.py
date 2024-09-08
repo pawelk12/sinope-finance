264765f7073bf98abd4890fbde64b23b55c68031
pawelk12 <pawel.krempa12@gmail.com>
2024-09-08 17:10:51 +0200

    Transfer history scrollbarframe interface enhancement

--- transfer_history.py
+++ transfer_history.py
@@ -1,31 +1,67 @@
 import customtkinter as ctk
+from tkinter import PhotoImage
 
-class TransferHistoryWidgets(ctk.CTkScrollableFrame):
+class TransferHistoryWidgets(ctk.CTkFrame):
     def __init__(self, master, mainframe, history):
         super().__init__(master)
-        self.configure(width=500, height=300)
         self.parent = mainframe
 
-        goBackButton = ctk.CTkButton(self, text="<-Back to Home",command=self.goBack)
-        goBackButton.grid(row=0,column=0,sticky="w")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        pathToArrow = "resources/arrowLeft.png"
 
-        titleLabel = ctk.CTkLabel(self, text="Transfer history",font=("Arial",20))
-        titleLabel.grid(row=1,column=0,columnspan=2,padx=20)
+        arrowImage = PhotoImage(file=pathToArrow)
+        goBackButton = ctk.CTkButton(self,
+                                    text="Home   ",
+                                    image=arrowImage,
+                                    fg_color="transparent",
+                                    corner_radius=30,
+                                    border_width=2,
+                                    border_spacing=6,
+                                    border_color="#3d9bd7",command=self.goBack)
+        goBackButton.grid(row=0,column=0,sticky="w",pady=3)
+
+        titleLabel = ctk.CTkLabel(self, text="Transfer history",font=("Arial",32))
+        titleLabel.grid(row=1,column=0,columnspan=2,pady=20)
+
+        self.transferHistoryFrame = ctk.CTkScrollableFrame(self, fg_color="transparent")
+        self.transferHistoryFrame.configure(width=900, height=450)
+
+        self.transferHistoryFrame.grid_columnconfigure(0, weight=1)
+        self.transferHistoryFrame.grid_columnconfigure(1, weight=1)
+        self.transferHistoryFrame.grid_columnconfigure(2, weight=1)
+        self.transferHistoryFrame.grid_columnconfigure(3, weight=1)
+
+        if not history:
+            infoLabel = ctk.CTkLabel(self.transferHistoryFrame,text="    Your transfer history is empty.",font=("Arial",18),padx=20)
+            infoLabel.grid(row=0,column=0,columnspan=4)
 
         i = 0
         for record in history:
             if (self.parent.account.BankAccNum == record[1]): # user sent money
-                sent = str(record[4]) + "\t" +str("{:.2f}".format(record[3])) + " PLN   sent to: " + str(record[2])
-                recordLabel = ctk.CTkLabel(self,text=sent,padx=20)
-            else:
-                received = str(record[4]) + "\t" +str("{:.2f}".format(record[3])) + " PLN   received from: " + str(record[1])
-                recordLabel = ctk.CTkLabel(self,text=received,padx=20)
-                recordLabel.configure(text_color = "#1a8e05")
-            recordLabel.grid(row=2+i,column=0,sticky="w")
+                dateLabel = ctk.CTkLabel(self.transferHistoryFrame,text=str(record[4]),font=("Arial",18),padx=20)
+                amountLabel = ctk.CTkLabel(self.transferHistoryFrame,text="-"+str("{:.2f}".format(record[3])+" PLN"),font=("Arial",18),padx=20)
+                sentLabel = ctk.CTkLabel(self.transferHistoryFrame,text="sent to",font=("Arial",18),padx=20)
+                receiverNumLabel = ctk.CTkLabel(self.transferHistoryFrame,text=str(record[2]),font=("Arial",18),padx=20)
+                dateLabel.grid(row=i,column=0)
+                amountLabel.grid(row=i,column=1)
+                sentLabel.grid(row=i,column=2)
+                receiverNumLabel.grid(row=i,column=3)
+
+            elif (self.parent.account.BankAccNum == record[2]): #if user received money
+                dateLabel = ctk.CTkLabel(self.transferHistoryFrame,text=str(record[4]),font=("Arial",18),padx=20)
+                amountLabel = ctk.CTkLabel(self.transferHistoryFrame,text=str("{:.2f}".format(record[3])+" PLN"),font=("Arial",18),text_color = "#1a8e05",padx=20)
+                receivedLabel = ctk.CTkLabel(self.transferHistoryFrame,text="received from",font=("Arial",18),padx=20)
+                receiverNumLabel = ctk.CTkLabel(self.transferHistoryFrame,text=str(record[1]),font=("Arial",18),padx=20)
+                dateLabel.grid(row=i,column=0)
+                amountLabel.grid(row=i,column=1)
+                receivedLabel.grid(row=i,column=2)
+                receiverNumLabel.grid(row=i,column=3)
+
             i = i + 1
 
-
-        self.pack(expand = True)
+        self.transferHistoryFrame.grid(row=2,column=0,columnspan=2)
+        self.pack(expand = True,fill=ctk.BOTH)
 
     def goBack(self):
         self.pack_forget()
